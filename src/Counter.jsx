e060a58a608409f3649736c01c08e476ea04cb0c
Andres Rivera <Andres.F.Rivera@jpl.nasa.gov>
2017-02-04 18:25:21 -0800

    Counter into it's own module

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,23 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
 class Counter extends React.Component {
   constructor(props) {
     super(props);
     this.state = { secondsElapsed: 0 };
   }
+  componentDidMount() {
+    this.interval = setInterval(() => this.tick(), 1000);
+  }
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
   tick() {
     this.setState(prevState => ({
       secondsElapsed: prevState.secondsElapsed + 1,
     }));
   }
-  componentDidMount() {
-    this.interval = setInterval(() => this.tick(), 1000);
-  }
-  componetWillUnmount() {
-    clearInterval(this.interval);
-  }
-
   render() {
     return (
       <div>
@@ -26,10 +24,8 @@ class Counter extends React.Component {
     );
   }
 }
-PageTitle.propTypes = {
+Counter.propTypes = {
   title: React.PropTypes.string.isRequired,
 };
 
-ReactDOM.render(
-  <Counter title='Time from inception:' />,
-  document.getElementById('app'));
+export default Counter;
